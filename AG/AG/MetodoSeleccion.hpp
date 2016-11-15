4a1bd556c3fb603adec5a1dc49832d8683a0fbb4
Álvaro Lázaro Sevilla <alseether@gmail.com>
2016-11-15 18:20:00 +0100

    Creadas clases padre para los metodos del AG
    Se han añadido las clases abstractas de MetodoSeleccion, MetodoMutacion
    y MetodoCruce.
    Ademas, se han incluido estas clases como atributos de los parametros
    del AG

---
+++ AG/AG/MetodoSeleccion.hpp
@@ -0,0 +1,15 @@
+#ifndef METODOSELECCION_HPP
+#define METODOSELECCION_HPP
+
+#include"Poblacion.hpp"
+
+class MetodoSeleccion {
+public:
+
+	virtual void seleccionar(Poblacion &pob);
+
+private:
+
+};
+
+#endif
