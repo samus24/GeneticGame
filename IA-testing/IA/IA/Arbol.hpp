f9cc624f6a94ac01140e614685ad5c1a51f64fdd
Samu <slapuente24@gmail.com>
2017-03-05 22:10:15 +0100

    Clase arbol, falta un metodo estilo tostring

--- IA-testing/IA/IA/Arbol.hpp
+++ IA-testing/IA/IA/Arbol.hpp
@@ -15,6 +15,27 @@ public:
 	Arbol(N raiz) {
 		this->_raiz = new Nodo<N>(raiz, this->_raiz, Nodo.grados.get(raiz));
 	}
+
+	void insertaNodo(int padre, N elem, int pos) {
+		Nodo<N> buscado = buscaNodo(this->_raiz, 0, padre);
+		if (buscado != nullptr) {
+			buscado.addHijo(elem, pos);
+		}
+	}
+
+	Nodo<N> buscaNodo(Nodo<N> origen, int numeroActual, int numeroBuscado) {
+		if (numeroActual == numeroBuscado) return origen;
+		else if (origen.esTerminal()) return nullptr;
+		else {
+			int i = 0;
+			Nodo<N> buscado = nullptr;
+			while (buscado == nullptr && i < origen.getnHijos()) {
+				buscado = buscaNodo(origen.getHijos().at(i), numeroActual + i + 1; numeroBuscado);
+				if (buscado == nullptr) ++i;
+			}
+		}
+		return buscado;
+	}
 private:
 	Nodo<N> _raiz;
 };
