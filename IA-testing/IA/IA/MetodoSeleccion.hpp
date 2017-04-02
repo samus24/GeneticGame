b42390bb7d28fd6404f5b3bb49287ae1fb82094e
Samu <slapuente24@gmail.com>
2017-04-02 18:30:39 +0200

    estandarizado el parametro tam

--- IA-testing/IA/IA/MetodoSeleccion.hpp
+++ IA-testing/IA/IA/MetodoSeleccion.hpp
@@ -14,22 +14,22 @@ public:
 class seleccionEstocastica : public metodoSeleccion {
 	
 	void seleccionar(poblacion pob, bool maximizar) {
-		std::vector<int> seleccionados(pob.tam);
+		std::vector<int> seleccionados(pob._tam);
 		double prob;
 		int posSuper;
-		double segmento = 1 / pob.tam; //El segmento es 1/N
+		double segmento = 1 / pob._tam; //El segmento es 1/N
 		prob = myRandom::getRandom(0.f, segmento);
-		for (std::size_t i = 0; i < pob.tam; ++i) {
+		for (std::size_t i = 0; i < pob._tam; ++i) {
 			posSuper = 0;
-			while ((posSuper < pob.tam) && (prob > pob.individuos[posSuper].getPuntAcum())) {
+			while ((posSuper < pob._tam) && (prob > pob.individuos[posSuper].getPuntAcum())) {
 				posSuper++;
 			}
 			seleccionados[i] = posSuper;
 			prob += segmento;
 		}
 
-		std::vector<Cromosoma> nuevaPob(pob.tam);
-		for (std::size_t i = 0; i < pob.tam; ++i) {
+		std::vector<Cromosoma> nuevaPob(pob._tam);
+		for (std::size_t i = 0; i < pob._tam; ++i) {
 			nuevaPob[i] = pob.individuos[seleccionados[i]];
 		}
 
@@ -44,18 +44,18 @@ class seleccionEstocastica : public metodoSeleccion {
 
 class seleccionRuleta : public metodoSeleccion {
 	void seleccionar(poblacion pob, bool maximizar) {
-		std::vector<int> seleccionados(pob.tam);
+		std::vector<int> seleccionados(pob._tam);
 		double prob;
 		int posSuper;
-		for (std::size_t i = 0; i < pob.tam; ++i) {
+		for (std::size_t i = 0; i < pob._tam; ++i) {
 			prob = myRandom::getRandom(0.f, 1.f); //un double entre 0.0 y 1.0
 			posSuper = 0;
-			while ((posSuper < pob.tam) && (prob > pob.individuos[posSuper].getPuntAcum()))
+			while ((posSuper < pob._tam) && (prob > pob.individuos[posSuper].getPuntAcum()))
 				posSuper++;
 			seleccionados[i] = posSuper;
 		}
-		std::vector<Cromosoma> nuevaPob(pob.tam);
-		for (std::size_t i = 0; i < pob.tam; ++i) {
+		std::vector<Cromosoma> nuevaPob(pob._tam);
+		for (std::size_t i = 0; i < pob._tam; ++i) {
 			nuevaPob[i] = pob.individuos[seleccionados[i]];
 		}
 
@@ -70,16 +70,16 @@ class seleccionRuleta : public metodoSeleccion {
 class seleccionTorneo : public metodoSeleccion {
 
 	void seleccionar(poblacion pob, bool maximizar) {
-		std::vector<int> seleccionados(pob.tam);
+		std::vector<int> seleccionados(pob._tam);
 		int posMejor, indexA, indexB, indexC;
 
-		for (std::size_t i = 0; i < pob.tam; ++i) {
-			indexA = (int)(myRandom::getRandom(0.f, 1.f) * pob.tam);
+		for (std::size_t i = 0; i < pob._tam; ++i) {
+			indexA = (int)(myRandom::getRandom(0.f, 1.f) * pob._tam);
 			do {
-				indexB = (int)(myRandom::getRandom(0.f, 1.f) * pob.tam);
+				indexB = (int)(myRandom::getRandom(0.f, 1.f) * pob._tam);
 			} while (indexB == indexA);
 			do {
-				indexC = (int)(myRandom::getRandom(0.f, 1.f) * pob.tam);
+				indexC = (int)(myRandom::getRandom(0.f, 1.f) * pob._tam);
 			} while (indexB == indexC || indexA == indexC);
 
 			if (maximizar) {
@@ -120,8 +120,8 @@ class seleccionTorneo : public metodoSeleccion {
 			}
 			seleccionados[i] = posMejor;
 		}
-		std::vector<Cromosoma> nuevaPob(pob.tam);
-		for (std::size_t i = 0; i < pob.tam; ++i) {
+		std::vector<Cromosoma> nuevaPob(pob._tam);
+		for (std::size_t i = 0; i < pob._tam; ++i) {
 			nuevaPob[i] = pob.individuos[seleccionados[i]];
 		}
 
