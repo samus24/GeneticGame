a05c3220ecebbebadcd79e7915744315cd26f2b9
Álvaro Lázaro Sevilla <alseether@gmail.com>
2017-05-10 18:55:05 +0200

    Mas bugs corregidos y mutaciones nuevas

--- AG/AG/Main.cpp
+++ AG/AG/Main.cpp
@@ -12,18 +12,18 @@
 int main(){
 	Parametros p;
 	p.tamPob = 30;							// Tamano de la poblacion
-	p.iteraciones = 30;						// Numero maximo de generaciones
+	p.iteraciones = 60;						// Numero maximo de generaciones
 	p.minNodos = 10;						// Numero minimo de nodos iniciales
 	p.maxNodos = 40;						// Numero maximo de nodos iniciales
 	p.densidad = 0.03;						// Densidad de aristas inciales
-	p.elitismo = true;						// Elitismo (guarda a los mejores)
+	p.elitismo = false;						// Elitismo (guarda a los mejores)
 	p.bloating = true;						// Bloating	(evita grafos demasiado grandes)
 	p.contractividad = false;				// Contractividad (descarta generaciones malas)
 	p.probCruce = 0.6;						// Probabilidad de cruce
 	p.probMutacion = 0.02;					// Probabilidad de mutacion
 	p.seleccion = new SeleccionTorneo();	// Metodo de seleccion (Ver "MetodoSeleccion.hpp")
 	p.cruce = new CruceMonopunto();			// Metodo de cruce (Ver "MetodoCruce.hpp")
-	p.mutacion = new MutacionArista();		// Metodo de mutacion (Ver "MetodoMutacion.hpp")
+	p.mutacion = new MutacionCombinada();		// Metodo de mutacion (Ver "MetodoMutacion.hpp")
 
 	ParametrosEval pEval;
 	pEval.nodosOptimos = 25;
