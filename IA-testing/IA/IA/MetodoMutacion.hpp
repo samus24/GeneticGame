1bc2b59cf8a5f9b7af1d59a355f23859eb892d68
Samu <slapuente24@gmail.com>
2017-05-13 12:50:42 +0200

    Añadida mutación combinada sin testear
    Cuando acabe la ejecución que estoy lanzando, la pruebo, pero la dejo
    subida por si alguien la prueba antes

--- IA-testing/IA/IA/MetodoMutacion.hpp
+++ IA-testing/IA/IA/MetodoMutacion.hpp
@@ -36,6 +36,7 @@ class mutacionFuncion : public metodoMutacion {
 public:
 	void mutar(Cromosoma* c, TipoArbol tipo) {
 		Arbol arb = c->getGenotipo(tipo);
+		arb.reparaReferencias();
 		Nodo n;
 		n = arb.getNodoFuncionAleatorio();
 		if (n.getPadre() != nullptr) {
@@ -76,6 +77,52 @@ public:
 
 	std::string toString() {
 		return "Terminal";
+	}
+};
+
+class mutacionCombinada : public metodoMutacion {
+public:
+	void mutar(Cromosoma* c, TipoArbol tipo) {
+		Arbol arb = c->getGenotipo(tipo);
+		arb.reparaReferencias();
+		Nodo n;
+		int r = myRandom::getRandom(0, 2); //elegimos al azar qué mutación vamos a usar
+		if (r == 0) {
+			n = arb.getNodoFuncionAleatorio();
+			if (n.getPadre() != nullptr) {
+				Nodo nuevo = arb.creaArbol(n.getPadre(), &n, arb.getProfMin(), arb.getProfMax(), n.getPos(), tipo);
+				n.getPadre()->addHijo(nuevo, n.getPos());
+				arb.reparaReferencias();
+				arb.actualizaNumNodos();
+				c->setGenotipo(arb, tipo);
+			}
+		}
+		if (r == 1) {
+			if (n.getPadre() != nullptr) {
+				if (n.getNhijos() == 2){
+					// Solo se pueden intercambiar nodos con 2 hijos, el resto son terminales o no hay mas con los mismos hijos
+					Operacion elem;
+					do{
+						elem = Nodo::getNoTerminalAleatorio(tipo);
+					} while (elem == n.getElem() || GRADOS[elem] != 2);
+					n.setElem(elem);
+					arb.actualizaNumNodos();
+					c->setGenotipo(arb, tipo);
+				}
+			}
+		}
+		else {
+			n = arb.getTerminalAleatorio();
+			if (n.getPadre() != nullptr) {
+				Operacion elem;
+				do{
+					elem = Nodo::getTerminalAleatorio(tipo);
+				} while (elem == n.getElem());
+				n.setElem(elem);
+				arb.actualizaNumNodos();
+				c->setGenotipo(arb, tipo);
+			}
+		}
 	}
 };
 
